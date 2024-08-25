6758e6ca050ef5f1bdf721de10d6377a0bb03172
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-08-25 01:15:42 +0100

    style: improve sidebar list
    add icons to each entry in the sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,44 @@
 import styles from './SidebarStyles.module.css';
+import { MdHome, MdHistory, MdAccountCircle, MdOutlinePlaylistPlay, MdOutlinePersonOutline, MdAlbum, MdFavoriteBorder,
+    MdOutlinePodcasts, MdOutlineMenuBook, MdOutlineNewReleases, MdInsertChartOutlined, MdOutlineMusicNote,
+    MdOutlineSettings, MdHelpOutline, MdOutlineLineWeight} from "react-icons/md";
 
 const Sidebar = () => {
     return (
         <div className={styles.sidebar}>
             <div className={styles.section}>
                 <ul>
-                    <li>Home</li>
-                    <li>History</li>
-                    <li>DJ</li>
+                    <li><MdHome className={styles.icon}/> Home</li>
+                    <li><MdHistory className={styles.icon}/>History</li>
+                    <li><MdAccountCircle className={styles.icon}/>Profile</li>
                 </ul>
             </div>
             <div className={styles.section}>
-                <h3 className={styles.sectionHeading}>Library</h3>
+            <h3 className={styles.sectionHeading}>Library</h3>
                 <ul>
-                    <li>Playlists</li>
-                    <li>Artists</li>
-                    <li>Albums</li>
-                    <li>Liked Songs</li>
-                    <li>Your Podcasts</li>
-                    <li>Your Audiobooks</li>
+                    <li><MdOutlinePlaylistPlay className={styles.icon}/>Playlists</li>
+                    <li><MdOutlinePersonOutline className={styles.icon}/>Artists</li>
+                    <li><MdAlbum className={styles.icon}/>Albums</li>
+                    <li><MdFavoriteBorder className={styles.icon}/>Liked Songs</li>
+                    <li><MdOutlinePodcasts className={styles.icon}/>Your Podcasts</li>
+                    <li><MdOutlineMenuBook className={styles.icon}/>Your Audiobooks</li>
                 </ul>
             </div>
             <div className={styles.section}>
                 <h3 className={styles.sectionHeading}>Discover</h3>
                 <ul>
-                    <li>New Releases</li>
-                    <li>Charts</li>
-                    <li>Genres</li>
-                    <li>New Podcasts</li>
-                    <li>New Audiobooks</li>
+                    <li><MdOutlineNewReleases className={styles.icon}/>New Releases</li>
+                    <li><MdInsertChartOutlined className={styles.icon}/>Charts</li>
+                    <li><MdOutlineMusicNote className={styles.icon}/>Genres</li>
+                    <li><MdOutlinePodcasts className={styles.icon}/>New Podcasts</li>
+                    <li><MdOutlineMenuBook className={styles.icon}/>New Audiobooks</li>
                 </ul>
             </div>
             <div className={styles.section}>
                 <ul>
-                    <li>Account Settings</li>
-                    <li>Help &amp; Support</li>
-                    <li>What&apos;s New</li>
+                    <li><MdOutlineSettings className={styles.icon}/>Account Settings</li>
+                    <li><MdHelpOutline className={styles.icon}/>Help &amp; Support</li>
+                    <li><MdOutlineLineWeight className={styles.icon}/>What&apos;s New</li>
                 </ul>
             </div>
         </div>
