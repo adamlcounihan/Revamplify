a665e12cd1e685612989b98f0c52c73d1cdb4fc6
Adam Counihan <137104673+adamlcounihan@users.noreply.github.com>
2024-09-26 01:12:48 +0100

    feat: add check if already authenticated

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import styles from './LoginPageStyles.module.css';
 import logo from '/logo.png';
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/check-auth', { withCredentials: true });
+
+                if (response.data.authenticated) {
+                    // If authenticated, redirect to the main page
+                    navigate('/main');
+                }
+            } catch (error) {
+                console.error('User not authenticated, staying on login page', error);
+            }
+        };
+
+        checkAuth();
+    }, [navigate]);
+
     const handleLogin = () => {
         window.location.href = 'http://localhost:5000/login';
     };
